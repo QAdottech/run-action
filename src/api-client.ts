fed6c53f44f4fa1fa8526e8aec66e8d6dda63fad
Tobias Törnros <t.tornros@gmail.com>
2025-01-27 11:15:24 +0100

    format

--- src/api-client.ts
+++ src/api-client.ts
@@ -2,53 +2,53 @@ import fetch from "node-fetch";
 import * as core from "@actions/core";
 
 export interface RunDetails {
-  id: string;
-  shortId: string;
+	id: string;
+	shortId: string;
 }
 
 export interface Payload {
-  trigger: string;
-  actor: string;
-  branch: string;
-  commitHash: string;
-  repository: string;
-  testPlanShortIds?: string[];
+	trigger: string;
+	actor: string;
+	branch: string;
+	commitHash: string;
+	repository: string;
+	testPlanShortIds?: string[];
 }
 
 export interface ApiResponse {
-  success?: boolean;
-  run?: RunDetails;
+	success?: boolean;
+	run?: RunDetails;
 }
 
 export const triggerQATechRun = async (
-  apiUrl: string,
-  apiToken: string,
-  payload: Payload
+	apiUrl: string,
+	apiToken: string,
+	payload: Payload,
 ): Promise<ApiResponse> => {
-  try {
-    const response = await fetch(apiUrl, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${apiToken}`,
-      },
-      body: JSON.stringify(payload),
-    });
+	try {
+		const response = await fetch(apiUrl, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${apiToken}`,
+			},
+			body: JSON.stringify(payload),
+		});
 
-    if (!response.ok) {
-      throw new Error(
-        `HTTP error! status: ${response.status} - ${await response.text()}`
-      );
-    }
+		if (!response.ok) {
+			throw new Error(
+				`HTTP error! status: ${response.status} - ${await response.text()}`,
+			);
+		}
 
-    const apiResponse = (await response.json()) as ApiResponse;
-    return apiResponse;
-  } catch (error) {
-    if (error instanceof Error) {
-      core.error(`Error during fetch operation: ${error.message}`);
-    } else {
-      core.error("An unknown error occurred during fetch");
-    }
-    throw error;
-  }
+		const apiResponse = (await response.json()) as ApiResponse;
+		return apiResponse;
+	} catch (error) {
+		if (error instanceof Error) {
+			core.error(`Error during fetch operation: ${error.message}`);
+		} else {
+			core.error("An unknown error occurred during fetch");
+		}
+		throw error;
+	}
 };
