81869c5178728c564bfb28fb30c7868571b6c996
Tobias Törnros <t.tornros@gmail.com>
2025-07-04 13:04:00 +0200

    build

--- dist/index.js
+++ dist/index.js
@@ -39421,6 +39421,7 @@ async function run() {
         if (result.run) {
             core.setOutput("run_created", "true");
             core.setOutput("run_short_id", result.run.shortId);
+            core.setOutput("run_url", result.run.url);
             core.info(`QA.tech run started with ID: ${result.run.shortId}${result.run.testPlan
                 ? `, Test Plan: ${result.run.testPlan.name} with ID: ${result.run.testPlan.short_id}`
                 : ""}`);
